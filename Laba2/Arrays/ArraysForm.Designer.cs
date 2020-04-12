b1166302f497efc278a3051201183e5e1146f188
Vladimir Vlasoff <holdencolfild19@gmail.com>
2020-04-12 21:02:45 +0300

    Laba 2

--- Laba2/Arrays/ArraysForm.Designer.cs
+++ Laba2/Arrays/ArraysForm.Designer.cs
@@ -39,11 +39,16 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
             this.resultLabel2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -92,7 +97,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(742, 9);
+            this.label4.Location = new System.Drawing.Point(693, 9);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(181, 13);
             this.label4.TabIndex = 5;
@@ -129,7 +134,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(470, 28);
+            this.label6.Location = new System.Drawing.Point(388, 28);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(59, 13);
             this.label6.TabIndex = 9;
@@ -137,32 +142,24 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(473, 71);
+            this.textBox3.Location = new System.Drawing.Point(391, 45);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(212, 190);
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(243, 216);
             this.textBox3.TabIndex = 10;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(473, 52);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(94, 13);
-            this.label7.TabIndex = 11;
-            this.label7.Text = "Введите матрицу";
             // 
             // resultLabel2
             // 
             this.resultLabel2.AutoSize = true;
-            this.resultLabel2.Location = new System.Drawing.Point(473, 364);
+            this.resultLabel2.Location = new System.Drawing.Point(391, 364);
             this.resultLabel2.Name = "resultLabel2";
             this.resultLabel2.Size = new System.Drawing.Size(0, 13);
             this.resultLabel2.TabIndex = 12;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(473, 318);
+            this.button2.Location = new System.Drawing.Point(391, 318);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(104, 23);
             this.button2.TabIndex = 13;
@@ -172,7 +169,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(473, 267);
+            this.button3.Location = new System.Drawing.Point(391, 267);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(212, 23);
             this.button3.TabIndex = 14;
@@ -182,21 +179,77 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(473, 292);
+            this.textBox4.Location = new System.Drawing.Point(391, 292);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(212, 20);
             this.textBox4.TabIndex = 15;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog1_FileOk);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(944, 28);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Задание 2";
+            this.label7.Click += new System.EventHandler(this.Label7_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(884, 63);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(89, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Выберите файл:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(979, 58);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Выбрать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(887, 98);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(167, 23);
+            this.button5.TabIndex = 21;
+            this.button5.Text = "Поменять слова местами";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(750, 127);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(487, 332);
+            this.textBox5.TabIndex = 22;
             // 
             // ArraysForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1249, 541);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.resultLabel2);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox2);
@@ -228,10 +281,15 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label resultLabel2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
--- Laba2/Arrays/ArraysForm.cs
+++ Laba2/Arrays/ArraysForm.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Windows.Forms;
 
 namespace Laba2.Arrays
 {
     public partial class ArraysForm : Form
     {
+
+        private int[,] array;
+        private string fileName;
+
         public ArraysForm()
         {
             InitializeComponent();
@@ -136,7 +142,7 @@ namespace Laba2.Arrays
                     // складываем
                     sum += arr[j, i];
                     // выводим
-                    if (j == size)
+                    if (j == size - 1)
                     {
                         result.Add($"Сумма элементов столбца {i + 1} = {sum}\n");
                         
@@ -178,8 +184,21 @@ namespace Laba2.Arrays
             int size;
             bool bSize = int.TryParse(textBox4.Text, out size);
             if (bSize)
-            {
-                textBox3.Text = readRandomArray(size);
+            {              
+                Random random = new Random();
+                string matrix = "";
+                array = new int[size, size];
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        int element = random.Next(-5, 20);
+                        array[i, j] = element;
+                        matrix += element + "\t";
+                    }
+                    matrix += Environment.NewLine;
+                }
+                textBox3.Text = matrix;
             }
             else
             {
@@ -197,30 +216,14 @@ namespace Laba2.Arrays
             resultLabel2.Text = "";
             if(textBox3.Text != null)
             {
-                int size = Convert.ToInt32(Math.Sqrt(textBox3.Text.Length));
-                var strArr = textBox3.Text.Split(' ', '\n', '\t');
-                var arr = new int[strArr.Length];
-                int index = 0;
-                var twoDimArr = new int[size, size];
-                for(int i = 0;i < arr.Length; i++)
-                {
-                    int temp = int.Parse(strArr[i]);
-                    arr[i] = temp;
-                }
-                for (int i = 0; i < size; i++)
-                {
-                    for (int j = 0; j < size; j++)
-                    {
-                        twoDimArr[i, j] = arr[index];
-                        index++;
-                    }
-                }
-                var results = sumInColumnWithoutNegatives(twoDimArr, size);
+                var results = sumInColumnWithoutNegatives(array, 
+                    (int)Math.Sqrt(array.Length));
                 foreach (String result in results)
                 {
                     resultLabel2.Text += result + "\n";
                 }
-                
+                resultLabel2.Text += $"Минимум среди сумм модулей элементов диагоналей,\n" +
+                    $"параллельных побочной диагонали матрицы: {minOfSumDiag(array)}";
             }
             else
             {
@@ -230,6 +233,123 @@ namespace Laba2.Arrays
                         MessageBoxIcon.Warning
                         );
             }
+        }
+
+        private int minOfSumDiag(int[,] arr)
+        {
+            int size = (int)Math.Sqrt(arr.Length);
+            int summin = 0;
+            for (int i = 0; i < size - 1; i++) //левый верхний треугольник 
+            {
+                int sum = 0;
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j != size - 1)
+                    {
+                        sum += Math.Abs(arr[i - j, j]);
+                    }
+                    if ((i == 0) || (summin > sum))
+                    {
+                        summin = sum;
+                    }
+                    
+                }
+            }
+            for (int i = 1; i < size - 1; i++) //правый нижний треугольник 
+            {
+                int sum = 0;
+                for (int j = 0; j < size - i; j++)
+                {
+                    sum += Math.Abs(arr[i + j, size - i - j]);
+                }
+                if (summin > sum)
+                {
+                    summin = sum;
+                }
+            }
+            if (Math.Abs(array[size - 1, size - 1]) < summin)
+            {
+                summin = Math.Abs(arr[size - 1, size - 1]);
+            }
+            return summin;
+        }
+
+        private string shuffleWords(string filePath)
+        {
+            var sb = new StringBuilder();
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                // читать будем построчно
+                string line; // собсно строка
+                // если не дошли до конца
+                while ((line = sr.ReadLine()) != null)
+                {
+                    // то делим строку на слова
+                    string[] words = line.Split(new char[] { ' ', ',', '.' });
+                    // проходимся по ним
+                    for (int i = 0; i < words.Length - 1; i += 2)
+                    {
+                        // и меняем местами
+                        sb.Append(words[i + 1] + " " + words[i] + " ");
+                    }
+                    // на случай, если количество слов в строке нечетное
+                    if (words.Length % 2 != 0)
+                    {
+                        // просто выводим в конце последнее слово
+                        sb.Append(words[words.Length - 1]);
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+
+        private void Label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            var openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"E:\",
+                Title = "Выберите текстовый файл",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+            }
+        }
+
+        private void OpenFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            if(fileName != null)
+            {
+                string shuffledText = shuffleWords(fileName);
+                textBox5.Text = shuffledText;
+            }
+            else
+            {
+                MessageBox.Show("Выберите файл!",
+                        "Ошибка!",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning
+                        );
+            }
+            
         }
     }
 }
